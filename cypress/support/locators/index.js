85644a9254ddf712f857d4b5259a45d92d231d02
kphlab <kritsana.pun@hlabconsulting.com>
2024-12-17 17:37:02 +0700

    Remove didn't use locator.

--- cypress/support/locators/index.js
+++ cypress/support/locators/index.js
@@ -13,4 +13,3 @@ export { default as FocusNotePage } from './focus-note.locator';
 export { default as ProblemListPage } from './problem-list.locator';
 export { default as PreAssessment } from './pre-assessment.locator';
 export { default as GraphicSheetPage } from './graphic-sheet.locator';
-export { default as Order } from './order.locator';
