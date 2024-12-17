11bb98bf0d4909f9b0853260597a54bfb79d6730
kphlab <kritsana.pun@hlabconsulting.com>
2024-12-17 10:41:54 +0700

    Change directory from e2e to integration. Add locator. Add more argument on randomANnumber.

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'uefcks',
   e2e: {
     baseUrl: "https://sit-y.cortexcloud.co",
     chromeWebSecurity: false,
     video: true,
     waitForAnimations: true,
     defaultCommandTimeout: 99000,
-    specPattern: 'cypress/e2e/**/*.cy.js',
+    specPattern: 'cypress/integration/**/*.cy.js',
   }
 });
